c2a2fbeaaee60fad965d1a3b71415f0909f7a910
Maxleu <72872036+Maxleu@users.noreply.github.com>
2021-03-17 21:51:48 -0400

    Add files via upload

---
+++ Meu.py
@@ -0,0 +1,5 @@
+print (' APP PARA CALCULAR AUMENTO DE SALÁRIO ')
+nome = input ('Nome do Funcionário: ')
+salario = float (input('Salário do Funcionario: '))
+aumento = float (input('Percetual do aumento: '))
+print ('Valor total é: ', salario + (salario * aumento / 100))
